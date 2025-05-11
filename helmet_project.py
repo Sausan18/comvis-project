9aa9a6f52338bad5cb9bdfabd64bbc808b77196c
tiffanyrizkika <tiffanyriz@mhs.usk.ac.id>
2025-05-12 06:29:35 +0700

    Update helmet_project.py

--- helmet_project.py
+++ helmet_project.py
@@ -149,9 +149,10 @@ else:
                 if cls_id == 1:  # Ganti 0 dengan ID class helm di model Anda
                     count_nonhelm2 += 1
                
-        st.write("test")
-        st.write(f"**Jumlah Deteksi (Dengan CLAHE - Helm): {count_helm1}**")
-        st.write(f"**Jumlah Deteksi (Dengan CLAHE - Non Helm): {count_nonhelm2}**")
+
         
 cap.release()
+
+st.write(f"**Jumlah Deteksi (Dengan CLAHE - Helm): {count_helm1}**")
+st.write(f"**Jumlah Deteksi (Dengan CLAHE - Non Helm): {count_nonhelm2}**")
 
