f7aa2199d0193998d48027ab3338fd6f901c5748
tiffanyrizkika <tiffanyriz@mhs.usk.ac.id>
2025-05-12 06:39:01 +0700

    Update helmet_project.py

--- helmet_project.py
+++ helmet_project.py
@@ -151,12 +151,12 @@ else:
                     count_nonhelm2 += 1
                
         with col3:
-            st.write(f"**Jumlah Deteksi (Tanpa CLAHE - Helm): {count_helm1}**")
-            st.write(f"**Jumlah Deteksi (Tanpa CLAHE - NonHelm): {count_nonhelm1}**")
+            st.markdown(f"**Jumlah Deteksi (Tanpa CLAHE - Helm): {count_helm1}**")
+            st.markdown(f"**Jumlah Deteksi (Tanpa CLAHE - NonHelm): {count_nonhelm1}**")
         
         with col4:
-            st.write(f"**Jumlah Deteksi (Dengan CLAHE - Helm): {count_helm2}**")
-            st.write(f"**Jumlah Deteksi (Dengan CLAHE - Non Helm): {count_nonhelm2}**")
+            st.markdown(f"**Jumlah Deteksi (Dengan CLAHE - Helm): {count_helm2}**")
+            st.markdown(f"**Jumlah Deteksi (Dengan CLAHE - Non Helm): {count_nonhelm2}**")
         
 cap.release()
 
